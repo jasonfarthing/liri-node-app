50495e09ade6c57cd81b9cd3e8115708a53b98a6
jasonfarthing <jasonfarthingisnow@gmail.com>
2017-11-21 22:06:49 -0800

    twitter and spotify working, omdb not :(

--- keys.js
+++ keys.js
@@ -1,22 +1,17 @@
-console.log('this is loaded');
-
 var twitterKeys = {
-  consumer_key: '<Mf6458kf1EpgW9wnWCj6l3n8B>',
-  consumer_secret: '<xsG3vbCa43ilEPlToUsPU73lDdHodxyToRsKn9WTqpW3OVwq1o>',
-  access_token_key: '<429425317-6jYM0YvVB8IKEfVHl7BjaWzLfTJLcVPzk7ORGviA>',
-  access_token_secret: '<9TWbv4ZztUae2jt2NUTQgqPnYL7V3Nzt4OVZOYPKgmAPG>',
+  consumer_key: 'Mf6458kf1EpgW9wnWCj6l3n8B',
+  consumer_secret: 'xsG3vbCa43ilEPlToUsPU73lDdHodxyToRsKn9WTqpW3OVwq1o',
+  access_token_key: '429425317-6jYM0YvVB8IKEfVHl7BjaWzLfTJLcVPzk7ORGviA',
+  access_token_secret: '9TWbv4ZztUae2jt2NUTQgqPnYL7V3Nzt4OVZOYPKgmAPG',
 }
 
-module.exports = twitterKeys;
+
 
 //---------------------
 
-var Spotify = require('node-spotify-api');
-
-var spotify = new Spotify({
- id: '7d690525a14f4c7d99727767a2fb84a1d',
- secret: '710c11aab591439485293419d1d3f69d'
-});
+module.exports = {
+    twitterKeys
+};
 
 // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
 //  if (err) {
@@ -27,15 +22,14 @@ var spotify = new Spotify({
 // });
 
 
-
-spotify
-.request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-.then(function(data) {
-  console.log(data); 
-})
-.catch(function(err) {
-  console.error('Error occurred: ' + err); 
-});
+// spotify
+// .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+// .then(function(data) {
+//   console.log(data); 
+// })
+// .catch(function(err) {
+//   console.error('Error occurred: ' + err); 
+// });
 
 
 //----------------------
--- liri.js
+++ liri.js
@@ -1,10 +1,102 @@
+var Twitter = require('twitter');
+var { twitterKeys } = require('./keys.js');
+var Spotify = require("node-spotify-api")
+var omdb = require('omdbapi');
 
-var myTwitterFeed = require('./keys.js');
-
-for (var key in myTwitterFeed){
-    console.log("hello");
+var spotify = new Spotify({
+    id: "7d690525a14f4c7d99727767a2fb84a1",
+    secret: "710c11aab591439485293419d1d3f69d"
+});
+//Twitter------------------- it's working
+var getMyTweets = function () {
+    var client = new Twitter(twitterKeys);
+    var params = {
+        screen_name: "thebudrows",
+        count: 20
+    };
+    client.get("statuses/user_timeline", params, function (error, tweets, response) {
+        for (var index = 0; index < tweets.length; index++) {
+            console.log("-----------------------")
+            console.log("Tweet by: ", tweets[index].user.screen_name)
+            console.log("Date Tweeted: ", tweets[index].created_at)
+            console.log("Read it: ", tweets[index].text)
+            console.log("-----------------------")
+        }
+    })
 }
+// Spotify ------------------- it's working
+var getMeSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "The Sign"
+    }
+    spotify.search(
+        {
+            type: "track",
+            query: songName
+        },
+        function (error, data) {
+            if (error) {
+                console.log("error happened : ", error);
+                return;
+            }
+            // console.log("I am song data", data)
+            var songs = data.tracks.items
+            console.log("-------------------------")
+            console.log("Song name: ", songs[0].name)
+            console.log("Artist: ", songs[0].artists[0].name)
+            console.log("Album: ", songs[0].album.name)
+            console.log("Preview URL: ", songs[0].album.external_urls.spotify)
+            console.log("-------------------------")
+        }
+    )
+}
+//OMDB not working :( --------------------------
+// NOTE: if leave this in the code, my other calls won't run???
+//-----------------------
+// var request = require("request");
+// var movieName = process.argv[2];
+// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+// var getMeMovie = function () {
+//     if (movieName == undefined) {
+//         movieName = "Mr. Nobody"
+//     }
+//     omdbSearch(
+//         {
+//             type: "movie",
+//             request: queryUrl
+//         },
+//         request(queryUrl, function (error, response, body) {
+//             if (!error && response.statusCode === 200) {
+//             }
+//         }));
+//     console.log(request);
+
+    var pick = function (caseData, functionData) {
+        switch (caseData) {
+            case "my-tweets":
+                getMyTweets();
+                break;
+            case "spotify-this-song":
+                getMeSpotify(functionData);
+                break;
+            case "movie-this":
+                getMeMovie(functionData);
+                break;
+            default:
+                console.log("LIRI says....Try Again")
+        }
+    }
+    var runThis = function (argOne, argTwo) {
+        pick(argOne, argTwo);
+    };
 
 
-var Spotify = require('node-spotify-api');
+    runThis(process.argv[2], process.argv[3]);
+
+
+
+
+    
+            // console.log('MOVIE TITLE = ' + JSON.parse(body).Title);
+            // console.log("The movie's release date is: " + JSON.parse(body).Year);
 
